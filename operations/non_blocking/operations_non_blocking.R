eeb2171ffe4007d0f70bffaac6bda6d32e973e12
Daniel Rakotomalala <rakdanielh@gmail.com>
2026-02-23 20:56:33 +0100

    Work on non blocking behavior

---
+++ operations/non_blocking/operations_non_blocking.R
@@ -0,0 +1,31 @@
+# Minimal & parallelized & non-blocking
+# Using: furrr:: & callr::
+
+# Packages ----------------------------------------------------------------
+
+library(dplyr)
+library(furrr)
+library(readr)
+library(callr)
+
+# Mask --------------------------------------------------------------------
+
+mask <- read_csv(file.path(path_mask, "mask.csv"))
+
+# Conversion_function -----------------------------------------------------
+
+load("./operations/conversion_function.rda")
+
+# Operation ---------------------------------------------------------------
+
+r_bg(function () {
+  plan(multisession, workers = 8)
+  mask %>% 
+    select(param_1 = input_dir, param_2 = input_file, 
+           param_3 = output_dir, param_4 = output_file) %>% 
+    future_pmap(.f = \(...) {
+      # error-robust version of conversion_function
+      out <- safely(conversion_function)(...)
+      if (is.null(out$error)) {NULL} else conditionMessage(out$error)
+    })  
+})
--- operations/parallelized/operations_parallelized.R
+++ operations/parallelized/operations_parallelized.R
@@ -14,3 +14,16 @@ mask <- read_csv(file.path(path_mask, "mask.csv"))
 # Conversion_function -----------------------------------------------------
 
 load("./operations/conversion_function.rda")
+
+# Operation ---------------------------------------------------------------
+
+plan(multisession, workers = 8)
+
+mask %>% 
+  select(param_1 = input_dir, param_2 = input_file, 
+         param_3 = output_dir, param_4 = output_file) %>% 
+  future_pmap(.f = \(...) {
+    # error-robust version of conversion_function
+    out <- safely(conversion_function)(...)
+    if (is.null(out$error)) {NULL} else conditionMessage(out$error)
+  })
